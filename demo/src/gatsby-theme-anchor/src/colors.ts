e7e529b9e5ce8265554734b2651bd73ba5cf9628
zinoun.badr@gmail.com <zinoun.badr@gmail.com>
2020-02-21 11:33:39 +0100

    change gatsby color theme

--- demo/src/gatsby-theme-anchor/src/colors.ts
+++ demo/src/gatsby-theme-anchor/src/colors.ts
@@ -1,10 +1,10 @@
 export default {
-    primaryDark: '#1976D2',
-    primary: '#2196F3',
+    primaryDark: '#03797D',
+    primary: '#07E8D0',
     primaryLight: '#BBDEFB',
     textAlt: '#FFFFFF',
     background: '#FFFFFF',
-    accent: '#FFC107',
+    accent: '#08FF7A',
     text: '#212121',
     textSecondary: '#757575',
   };
