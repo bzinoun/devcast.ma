7170282829547af5bbd2ac55ce9e8d35edcfa958
Sallah Kokaina <sallah.kokaina@gmail.com>
2020-04-15 23:01:29 +0100

    include subscription

--- demo/gatsby-config.js
+++ demo/gatsby-config.js
@@ -2,12 +2,6 @@ module.exports = {
   plugins: [
     'gatsby-plugin-mailchimp',
     {
-      resolve: 'gatsby-plugin-load-script',
-      options: {
-        src: '/mailchimp.js',
-      },
-    },
-    {
       resolve: 'gatsby-theme-anchor',
       options: {
         anchorRss: 'https://anchor.fm/s/127a53ac/podcast/rss',
